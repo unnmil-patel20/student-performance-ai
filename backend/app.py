9be29d6b46c3b8a45d536e86b72d18bdd3da2935
Unnmil Patel <unnmilpatel20@gmail.com>
2026-09-20 16:10:48 +0530

    Allow Frontend to backend

--- backend/app.py
+++ backend/app.py
@@ -14,7 +14,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://student-performance-q95usfhmy-personal-projects-0e43.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
